1f0167efe5dc3f9c65757b927cc4ea1adf44a432
Jignesh <jignesh.sharma.707@gmail.com>
2023-10-14 02:46:13 +0530

    zustand store

--- components/TopMenu.jsx
+++ components/TopMenu.jsx
@@ -1,3 +1,9 @@
+'use client';
+
+import Link from 'next/link';
+import { AiOutlineClose } from 'react-icons/ai';
+import { RiMenu3Fill } from 'react-icons/ri';
+import { MdEmail } from 'react-icons/md';
 const TopMenu = () => {
   return <div>TopMenu</div>;
 };
